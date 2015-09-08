9c1b92f0935a6c0d62abf4740186e5a003e57410
Nelly Moseki <dineo06.moseki@gmail.com>
2015-09-08 20:45:52 +0200

    Created 2 new files and modified .scss files and .info file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	watch = require('gulp-watch'),
-	browserSync = require('browser-sync'),
 	paths =  {
 		scss: '.sass/*.scss'
 	};
@@ -15,15 +14,9 @@ gulp.task('sass', function (){
 	 	 .pipe(gulp.dest('css')); //save the css to this destination
 });
 
-//Gulp peforming browser-sync task
-gulp.task('browser-sync', function(){
-	browserSync.init(['css/*.css', 'js/*.js'],{
-		proxy: 'http://thelady'
-	});
-});
 
 //Gulp performing watch task
-gulp.task('watch',['sass','browser-sync'], function(){
+gulp.task('watch',['sass'], function(){
 	gulp.watch('scss/*.scss', ['sass']);
 });
 
